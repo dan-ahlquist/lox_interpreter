172a7c6e60c7f6820718737c67ba6a9d0340ef50
Dan <dan@ahlquist.dev>
2020-06-27 14:10:41 -0500

    8.4 - Assignment

--- src/dev/ahlquist/lox_interp/main/Interpreter.java
+++ src/dev/ahlquist/lox_interp/main/Interpreter.java
@@ -93,6 +93,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return environment.get(expr.name);
     }
 
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
     /* Implement Stmt.Visitor<Void> */
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
