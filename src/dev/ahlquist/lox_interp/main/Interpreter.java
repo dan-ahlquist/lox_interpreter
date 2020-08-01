51285f2b07894867c33d2ade3bb2fd7b26d4107a
Dan <dan@ahlquist.dev>
2020-08-01 16:32:19 -0500

    Section 11.4 - Resolver

--- src/dev/ahlquist/lox_interp/main/Interpreter.java
+++ src/dev/ahlquist/lox_interp/main/Interpreter.java
@@ -1,12 +1,15 @@
 package dev.ahlquist.lox_interp.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private Map<Expr, Integer> locals = new HashMap<>();
 
     public Interpreter() {
         globals.define("clock", new LoxCallable() {
@@ -118,13 +121,29 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookupVariable(expr.name, expr);
+    }
+
+    private Object lookupVariable(Token name, Expr.Variable expr) {
+        Integer distance = locals.get(expr);
+        if(distance != null) {
+            return environment.getAt(name, distance);
+        } else {
+            return globals.get(name);
+        }
     }
 
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer depth = locals.get(expr);
+        if(depth != null) {
+            environment.assignAt(expr.name, value, depth);
+        } else {
+            globals.assign(expr.name, value);
+        }
+
         return value;
     }
 
@@ -278,5 +297,9 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return object.toString();
+    }
+
+    public void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
     }
 }
