d4dd8b3e7544b4f3c860d68bd143b31bd01fd6ee
dan <dan.ahlquist@gmail.com>
2020-08-15 07:49:20 -0500

    Chapter 13 - Inheritance

--- src/dev/ahlquist/lox_interp/main/Interpreter.java
+++ src/dev/ahlquist/lox_interp/main/Interpreter.java
@@ -117,6 +117,22 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitSuperExpr(Expr.Super expr) {
+        int depth = locals.get(expr);
+        LoxClass superclass = (LoxClass) environment.getAt("super", depth);
+
+        // 'this' is always one level above 'super'
+        LoxInstance object = (LoxInstance) environment.getAt("this", depth -1);
+
+        LoxFunction method = superclass.findMethod(expr.method.lexeme);
+        if(method == null) {
+            throw new RuntimeError(expr.method, "Undefined property '" + expr.method.lexeme + "'.");
+        }
+
+        return method.bind(object);
+    }
+
+    @Override
     public Object visitThisExpr(Expr.This expr) {
         return lookupVariable(expr.keyword, expr);
     }
@@ -253,6 +269,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
         environment.define(stmt.name.lexeme, null);
 
+        if(stmt.superclass != null) {
+            environment = new Environment(environment);
+            environment.define("super", superclass);
+        }
+
         Map<String, LoxFunction> methods = new HashMap<>();
         for(Stmt.Function method : stmt.methods) {
             LoxFunction function
@@ -261,6 +282,10 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         LoxClass klass = new LoxClass(stmt.name.lexeme, (LoxClass)superclass, methods);
+
+        if(superclass != null)
+            environment = environment.parent;
+
         environment.assign(stmt.name, klass);
         return null;
     }
